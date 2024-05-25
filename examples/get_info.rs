91d3309b7928bca123102ba91375ed4a4e81ea95
Ada Hieta <ada@hieta.fi>
2024-05-25 10:32:30 +0300

    example fixes

--- examples/get_info.rs
+++ examples/get_info.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<()> {
 
     let mut tmc = Tmc4671::new(spi);
 
-    let si_type = tmc.get_chip_info(CHIP_INFO_ADDRESS::SI_TYPE).await?;
+    let si_type = tmc.get_chip_info(CHIP_INFO_ADDRESS::SI_TYPE)?;
 
     let si_type_str = String::from_utf8_lossy(&si_type.to_le_bytes());
 
