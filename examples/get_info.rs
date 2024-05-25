653d5b91ebe63686b4b512362060b5292891ffb8
Ada Hieta <ada@hieta.fi>
2024-05-25 10:27:16 +0300

    example fixes

--- examples/get_info.rs
+++ examples/get_info.rs
@@ -1,27 +1,13 @@
-#![cfg(unix)]
 use anyhow::Result;
-use embedded_hal_async::spi::Operation::DelayNs;
-use embedded_hal_bus::spi::AtomicDevice;
-use embedded_hal_bus::util::AtomicCell;
-use rppal::gpio;
-use rppal::spi;
+use linux_embedded_hal::SpidevDevice;
 use tmc4671_rs::spi::constants::CHIP_INFO_ADDRESS;
 use tmc4671_rs::Tmc4671;
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let spi_bus = spi::Spi::new(
-        spi::Bus::Spi0,
-        spi::SlaveSelect::Ss1,
-        8_000_000,
-        spi::Mode::Mode3,
-    )?;
-    let bus_in_cell = AtomicCell::new(spi_bus);
+    let spi = SpidevDevice::open("/dev/spidev0.0")?;
 
-    let gpio = gpio::Gpio::new()?;
-    let mut cs_pin = gpio.get(24)?.into_output();
-    cs_pin.set_high();
-
-    let mut tmc = Tmc4671::new(AtomicDevice::new(&bus_in_cell, cs_pin, DelayNs(500))?);
+    let mut tmc = Tmc4671::new(spi);
 
     let si_type = tmc.get_chip_info(CHIP_INFO_ADDRESS::SI_TYPE).await?;
 
